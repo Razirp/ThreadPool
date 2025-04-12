4c9eb48a1812599f7a84c39233e2e443389a85e4
Razirp <73270042+Razirp@users.noreply.github.com>
2025-04-13 00:31:14 +0800

    [platform compatibility] for Linux and Mac OS

--- include/thread_pool.hpp
+++ include/thread_pool.hpp
@@ -24,10 +24,12 @@
 #include <shared_mutex>
 #include <future>
 
-#ifndef DLL_EXPORT
-#define THREAD_EXPORT __declspec(dllexport)
-#else
-#define THREAD_EXPORT __declspec(dllimport)
+#ifdef _WIN32
+    #ifndef DLL_EXPORT
+        #define THREAD_EXPORT __declspec(dllexport)
+    #else
+        #define THREAD_EXPORT __declspec(dllimport)
+    #endif
 #endif
 
 /**
@@ -44,7 +46,11 @@ namespace thread_utils
  * The thread_pool class provides a simple interface for executing tasks concurrently using a pool of worker threads.
  * The class allows submitting tasks to the thread pool, pausing and resuming the pool, and shutting down the pool.
  */
+#ifdef _WIN32
 class THREAD_EXPORT thread_pool {
+#else
+class thread_pool {
+#endif
 private:
     class worker_thread;        // 工作线程类
     enum class status_t : std::int8_t { 
